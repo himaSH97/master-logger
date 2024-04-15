a5919adb0139bb1a3c3278703aa5d40a0f5dfefb
himaSH97 <nipun946@gmail.com>
2024-04-15 15:35:08 +0530

    feat: adding user input

--- src/extension.ts
+++ src/extension.ts
@@ -2,31 +2,67 @@ import * as vscode from "vscode";
 import { getStatement } from "./utils/utils";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "master-logger.wrapInStatement",
-    () => {
-      const editor = vscode.window.activeTextEditor;
-      if (editor) {
-        const selection = editor.selection;
-        const selectedText = editor.document.getText(selection);
-        const totalLines = editor.document.lineCount;
+  let disposable = vscode.commands.registerCommand("master-logger.wrapInStatement", async () => {
+    const editor = vscode.window.activeTextEditor;
 
-        if (selectedText) {
-          const currentPosition = editor.selection.active;
-          let newPosition = currentPosition.with(currentPosition.line + 1, 0);
+    if (!editor) {
+      vscode.window.showErrorMessage("Editor not found!");
+      return;
+    }
 
-          editor.edit((editBuilder) => {
-            let logStatement = getStatement(editor) + "\n";
-            if (currentPosition.line + 1 === totalLines) {
-              logStatement = "\n"+ logStatement;
-            }
-            editBuilder.insert(newPosition, logStatement);
-          });
-          editor.selection = selection;
-        }
+    const insertSpaces = editor.options.insertSpaces;
+
+    if (insertSpaces) {
+      const options: vscode.QuickPickItem[] = [
+        { label: "Yes", description: "Enable tab indentation" },
+        { label: "No", description: "Keep using spaces for indentation" },
+      ];
+      const pick = await vscode.window.showQuickPick(options, {
+        placeHolder: "Do you want to enable tab indentation to continue using this extension?",
+      });
+
+      if (!pick) {
+        return;
+      }
+
+      if (pick.label === "No") {
+        vscode.window.showInformationMessage(`Please Enable tab indentation to continue using this extension`);
+        return;
+      }
+      
+      if (pick.label === "Yes") {
+        vscode.window.showInformationMessage(`Tab Indentation Enabled`);
+        await vscode.commands.executeCommand("editor.action.indentationToTabs");
       }
     }
-  );
+
+    const editorLanguage = editor.document.languageId;
+    if (editorLanguage !== "python") {
+      vscode.window.showErrorMessage("Only Python supported for now!");
+      return;
+    }
+
+    const selection = editor.selection;
+    const totalLines = editor.document.lineCount;
+    const selectedText = editor.document.getText(selection);
+
+    if (!selectedText) {
+      vscode.window.showInformationMessage("Selected text not found!");
+      return;
+    }
+
+    const currentPosition = editor.selection.active;
+    let newPosition = currentPosition.with(currentPosition.line + 1, 0);
+
+    editor.edit((editBuilder) => {
+      let logStatement = getStatement(editor) + "\n";
+      if (currentPosition.line + 1 === totalLines) {
+        logStatement = "\n" + logStatement;
+      }
+      editBuilder.insert(newPosition, logStatement);
+    });
+    editor.selection = selection;
+  });
 
   context.subscriptions.push(disposable);
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -20,33 +20,10 @@ export const lang = {
   rust: ",",
 };
 
-export const getStatement = (editor: vscode.TextEditor): string => {
-  const editorLanguage = editor.document.languageId;
-  let content = `"${parseFileName(editor.document.fileName)} : ${editor.selection.active.line + 1} : ${
-    editor.document.getText(editor.selection)
-  }"`;
-
-  if(editor.document.languageId === 'python'){
-    let pyStatement= getPythonStatement(editor);  
-    return pyStatement;
-  }else if (
-    editorLanguage === "javascript" ||
-    editorLanguage === "typescript"
-  ) {
-    return `console.log(${content}${lang[editorLanguage]} ${editor.document.getText(editor.selection)})`;
-  } else if (editorLanguage === "csharp") {
-    return `Console.WriteLine(${content} ${lang[editorLanguage]} ${editor.document.getText(editor.selection)})`;
-  } else if (editorLanguage === "java") {
-    //work heredsgvcsd
-
-    return `System.out.println(${content} ${lang[editorLanguage]} ${editor.document.getText(editor.selection)})`;
-  } else if (editorLanguage === "rust") {
-    return ` println(${content}${lang[editorLanguage]} ${editor.document.getText(editor.selection)})`;
-  }
-
-  return ` println(${content}, ${editor.document.getText(editor.selection)})`;
+export const getStatement = (editor: vscode.TextEditor): string | undefined => {
+  const pyStatement= getPythonStatement(editor);  
+  return pyStatement;
 };
-
 
 export const parseFileName = (fileName: string): string => {
   let fileNameWithoutPath = "file";
